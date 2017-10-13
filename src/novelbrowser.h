9d8604a36b669aaffe68eae1db699826518ba6a2
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-13 03:15:57 +0100

    Tidied & Improved Icon&Thumb loading
    jpegs still MIA

--- src/novelbrowser.h
+++ src/novelbrowser.h
@@ -4,33 +4,49 @@
 class NovelHeader
 {
 	private:
+		bool FileExists(std::string Path)
+		{
+			SceIoStat StatTemp;
+			return (sceIoGetstat(Path.c_str(), &StatTemp) >= 0);
+		}
+
 		vita2d_texture * LoadImageName(std::string Path)
 		{
 			//Takes name without type por exampler;  ux0:data/vnvita/saya no uta/icon
 			//Priority : High PNG -> High JPG -> Base PNG -> Base PNG
 
-			//JPEG Doesn't work at the moment. Find out why it crashes. 
-			
-			vita2d_texture* Texture;
+			//Jpeg's don't load and it's pissing me off. Argh!
+
+			vita2d_texture* Texture = NULL;
 			std::string PathTemp = Path;
 
 			// High PNG
-			Texture = vita2d_load_PNG_file((PathTemp.append("-high.png")).c_str());
+			PathTemp.append("-high.png");
+			if(FileExists(PathTemp))
+			{
+				Texture = vita2d_load_PNG_file((PathTemp).c_str());
+			}
 			
 			// High JPG
 			if(Texture == NULL)
 			{
 				PathTemp = Path;
 				PathTemp.append("-high.jpg");
-				//Texture = vita2d_load_JPEG_file(PathTemp.c_str());
-			}			
+				if(FileExists(PathTemp))
+				{
+					Texture = vita2d_load_JPEG_file(PathTemp.c_str());
+				}
+			}
 
 			// Low PNG
 			if(Texture == NULL)
 			{
 				PathTemp = Path;
 				PathTemp.append(".png");
-				Texture = vita2d_load_PNG_file(PathTemp.c_str());
+				if(FileExists(PathTemp))
+				{
+					Texture = vita2d_load_PNG_file(PathTemp.c_str());
+				}
 			}
 
 			// Low JPG
@@ -38,7 +54,10 @@ class NovelHeader
 			{
 				PathTemp = Path;
 				PathTemp.append(".jpg");
-				//Texture = vita2d_load_JPEG_file(PathTemp.c_str());
+				if(FileExists(PathTemp))
+				{
+					Texture = vita2d_load_JPEG_file(PathTemp.c_str());
+				};
 			}
 
 			return Texture;
@@ -54,13 +73,15 @@ class NovelHeader
 		vita2d_texture * iconPointer = LoadImageName(Path + "\\icon"); 
 		if(iconPointer != NULL)
 		{
+			vita2d_texture_set_filters(iconPointer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
 			this->Icon = std::shared_ptr<vita2d_texture>(iconPointer, vita2d_free_texture);
 		}
 
 		//Thumbnail / Preview
 		vita2d_texture * thumbPointer = LoadImageName(Path + "\\thumbnail"); 
 		if(thumbPointer != NULL)
-		{
+		{	
+			vita2d_texture_set_filters(thumbPointer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
 			this->Thumbnail = std::shared_ptr<vita2d_texture>(thumbPointer, vita2d_free_texture);
 		}
 	}	
