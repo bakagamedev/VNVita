73d3cd8c06e989c32cd6b4fdee8c602dde7e0655
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-13 01:13:05 +0100

    partial High-res icon support

--- src/novelbrowser.h
+++ src/novelbrowser.h
@@ -3,24 +3,62 @@
 
 class NovelHeader
 {
+	private:
+		vita2d_texture * LoadImageName(std::string Path)
+		{
+			//Takes name without type por exampler;  ux0:data/vnvita/saya no uta/icon
+			//Priority : High PNG -> High JPG -> Base PNG -> Base PNG
+
+			//JPEG Doesn't work at the moment. Find out why it crashes. 
+			
+			vita2d_texture* Texture;
+			std::string PathTemp = Path;
+
+			// High png
+			Texture = vita2d_load_PNG_file((PathTemp.append("-high.png")).c_str());
+			
+			// High JPG
+			if(Texture == NULL)
+			{
+				PathTemp = Path;
+				PathTemp.append("-high.jpg");
+				//Texture = vita2d_load_JPEG_file(PathTemp.c_str());
+			}			
+
+			// Low PNG
+			if(Texture == NULL)
+			{
+				PathTemp = Path;
+				PathTemp.append(".png");
+				Texture = vita2d_load_PNG_file(PathTemp.c_str());
+			}
+
+			// Low JPG
+			if(Texture == NULL)
+			{
+				PathTemp = Path;
+				PathTemp.append(".jpg");
+				//Texture = vita2d_load_JPEG_file(PathTemp.c_str());
+			}
+
+			return Texture;
+		}
+
 	public:
 	NovelHeader(std::string Path)
 	{
 		this->Name = Path;
 		this->Path = Path;
 
-		//Check if file exists before read?
-		std::string IconPath = Path.c_str();
-		IconPath.append("\\icon.png");
-		auto iconPointer = vita2d_load_PNG_file(IconPath.c_str());
+		//Icon
+		vita2d_texture * iconPointer = LoadImageName(Path + "\\icon"); 
 		if(iconPointer != NULL)
 		{
 			this->Icon = std::shared_ptr<vita2d_texture>(iconPointer, vita2d_free_texture);
 		}
-		//Thumbnail
-		std::string ThumbPath = Path.c_str();
-		ThumbPath.append("\\thumbnail.png");
-		auto thumbPointer = vita2d_load_PNG_file(ThumbPath.c_str());
+
+		//Thumbnail / Preview
+		vita2d_texture * thumbPointer = LoadImageName(Path + "\\thumbnail"); 
 		if(thumbPointer != NULL)
 		{
 			this->Thumbnail = std::shared_ptr<vita2d_texture>(thumbPointer, vita2d_free_texture);
