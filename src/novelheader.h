90d0ce4a6526c4c82392fcdd1507b91ec1bd25b4
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-13 17:15:44 +0100

    Width/height reading
    untested

--- src/novelheader.h
+++ src/novelheader.h
@@ -64,46 +64,60 @@ class NovelHeader
 		}
 
 	public:
-	NovelHeader(void);
+	
+		int Width = 256;
+		int Height = 192;
+		std::string Name;
+		std::string Path;
+		std::shared_ptr<vita2d_texture> Icon;
+		std::shared_ptr<vita2d_texture> Thumbnail;
 
-	NovelHeader(std::string Path, bool LoadImages)
-	{
-		this->Name = Path;
-		this->Path = Path;
+		NovelHeader(void);
 
-		auto InfoPath = Path + "\\info.txt";
-    	INIReader reader(InfoPath);
-    	if (reader.ParseError() < 0) {
-    		Name = Path;
-    		Name.append("#");
-    	}
-    	else
-    	{
-    		Name = reader.Get("", "title", Path);
-    	}
-
-		if(LoadImages)
+		NovelHeader(std::string Path, bool LoadImages)
 		{
-			//Icon
-			vita2d_texture * iconPointer = LoadImageName(Path + "\\icon"); 
-			if(iconPointer != NULL)
+			this->Name = Path;
+			this->Path = Path;
+
+			//Load name from info.txt
+			auto InfoPath = Path + "\\info.txt";
+	    	INIReader reader(InfoPath);
+	    	if (reader.ParseError() < 0) {
+	    		Name = Path;
+	    		Name.append("#");
+	    	}
+	    	else
+	    	{
+	    		Name = reader.Get("", "title", Path);
+	    	}
+
+	    	//Image size from img.ini
+			auto ImgPath = Path + "\\img.ini";
+	    	INIReader reader(InfoPath);
+			if (reader.ParseError() >= 0)
 			{
-				vita2d_texture_set_filters(iconPointer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
-				this->Icon = std::shared_ptr<vita2d_texture>(iconPointer, vita2d_free_texture);
+				Width = GetInteger("", "width", Width);
+				Height = GetInteger("", "height", Height);
 			}
 
-			//Thumbnail / Preview
-			vita2d_texture * thumbPointer = LoadImageName(Path + "\\thumbnail"); 
-			if(thumbPointer != NULL)
-			{	
-				vita2d_texture_set_filters(thumbPointer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
-				this->Thumbnail = std::shared_ptr<vita2d_texture>(thumbPointer, vita2d_free_texture);
-			}
-		}
-	}	
+			//Images
+			if(LoadImages)
+			{
+				//Icon
+				vita2d_texture * iconPointer = LoadImageName(Path + "\\icon"); 
+				if(iconPointer != NULL)
+				{
+					vita2d_texture_set_filters(iconPointer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
+					this->Icon = std::shared_ptr<vita2d_texture>(iconPointer, vita2d_free_texture);
+				}
 
-	std::string Name;
-	std::string Path;
-	std::shared_ptr<vita2d_texture> Icon;
-	std::shared_ptr<vita2d_texture> Thumbnail;
-};
+				//Thumbnail / Preview
+				vita2d_texture * thumbPointer = LoadImageName(Path + "\\thumbnail"); 
+				if(thumbPointer != NULL)
+				{	
+					vita2d_texture_set_filters(thumbPointer, SCE_GXM_TEXTURE_FILTER_LINEAR, SCE_GXM_TEXTURE_FILTER_LINEAR);
+					this->Thumbnail = std::shared_ptr<vita2d_texture>(thumbPointer, vita2d_free_texture);
+				}
+			}
+		}	
+}
