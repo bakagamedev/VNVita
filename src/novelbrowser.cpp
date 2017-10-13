ee0bc63f7c80793d077f10b505d718926ee3b8ea
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-13 15:13:14 +0100

    Shows blank screen on load

--- src/novelbrowser.cpp
+++ src/novelbrowser.cpp
@@ -73,7 +73,7 @@ std::string NovelBrowser::Run()
 		{
 			vita2d_start_drawing();
 			vita2d_clear_screen();
-			vita2d_pgf_draw_text(pgf, 30, 30, RGBA8(255,0,0,255), 2.0f, "bad. No directory for you.");
+			vita2d_pgf_draw_text(pgf, 0,25,RGBA8(255,0,0,255), 1.5f, "No directory!");
 			vita2d_end_drawing();
 			vita2d_swap_buffers();
 
--- src/novelmain.cpp
+++ src/novelmain.cpp
@@ -11,18 +11,26 @@ NovelMain::NovelMain(std::string LoadPath)
 
 void NovelMain::Run()
 {
-	//SceCtrlData GamePad, GamePadLast;
-	//while(GamePad.buttons == 0)
-	//{
-		//sceCtrlPeekBufferPositive(0, &GamePad, 1);
+	SceCtrlData GamePad, GamePadLast;
+	
+	bool Ready = false;
+	while(!Ready)
+	{
+		sceCtrlPeekBufferPositive(0, &GamePad, 1);
+		if((GamePad.buttons & SCE_CTRL_CIRCLE) && ((GamePadLast.buttons & SCE_CTRL_CIRCLE) == 0))
+		{
+			Ready = true;
+		}
 
 		vita2d_start_drawing();
 		vita2d_clear_screen();
 
-   		vita2d_draw_rectangle(0, 0, 960, 544, RGBA8(114, 137, 217, 255));
+		vita2d_draw_rectangle(0, 0, 960, 544, RGBA8(114, 137, 217, 255));
 		//vita2d_pgf_draw_text(pgf, 64,64, RGBA8(255,0,0,255), 1.0f, "Path.c_str()");
 
 		vita2d_end_drawing();
 		vita2d_swap_buffers();
-	//}
+
+		GamePadLast = GamePad;
+	}
 }
