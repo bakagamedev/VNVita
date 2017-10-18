030d1c6746f2e9931bc0e946d94f88ce3b3b8866
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-18 23:03:06 +0100

    String trims moved to stringhelper.h

--- src/vndsparse.cpp
+++ src/vndsparse.cpp
@@ -1,24 +1,5 @@
 #include "vndsparse.h"
 
-//https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
-//Move somewhere else
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-}
-
-// trim from both ends (in place)
-static inline void trim(std::string &s) {
-    ltrim(s);
-    rtrim(s);
-}
 
 VNDSParser::VNDSParser()
 {
--- src/vndsparse.h
+++ src/vndsparse.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "common.h"
 #include "vndsinstruction.h"
+#include "stringhelpers.h"
 
 class VNDSParser
 {
