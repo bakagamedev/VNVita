a38dc8cdce5b2424e25426420f87f984e213a12c
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-23 21:08:23 +0100

    Variable stuff
    this looks familar...

--- src/vndsvariable.h
+++ src/vndsvariable.h
@@ -1,6 +1,33 @@
 #pragma once
 
+enum class VNDSVariableType
+{
+	Integer,
+	String,
+};
+
+union VNDSVariableStore
+{
+	int Integer;
+	std::string String;
+}
+
 class VNDSVariable
 {
-	
+public:
+	std::string Name;
+	VNDSVariableStore Data;
+
+	VNDSVariable(std::string Name, std::string Data)
+	{
+		this->Name = Name;
+		this->Data = Data;
+		this->Type = VNDSVariableType::String;
+	}
+	VNDSVariable(std::string Name, int Data)
+	{
+		this->Name = Name;
+		this->Data = Data;
+		this->Type = VNDSVariableType::Integer;
+	}
 }
