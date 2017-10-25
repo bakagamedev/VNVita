841a04b776f97405287a14d3fc537d1b7bcea1ba
bakagamedev <noel.abercrombie@hotmail.com>
2017-10-25 17:06:15 +0100

    Did a thing

--- src/vndsvariable.h
+++ src/vndsvariable.h
@@ -12,6 +12,10 @@ union VNDSVariableData {
 
 class VNDSVariable
 {
+
+private:
+
+
 public:
 	VNDSVariableData Data;
 	VNDSVariableType Type;
@@ -21,11 +25,15 @@ public:
 		this->Data.Integer = Data;
 		this->Type = VNDSVariableType::Integer;
 	}
-};
 
+	bool IsInteger() const
+	{
+		return(this->Type == VNDSVariableType::Integer);
+	}
+};
 inline bool operator==(const VNDSVariable& lhs, const VNDSVariable& rhs)
 { 
-	if((lhs.Type == VNDSVariableType::Integer) && (rhs.Type == VNDSVariableType::Integer))
+	if((lhs.IsInteger()) && (rhs.IsInteger()))
 		return lhs.Data.Integer == rhs.Data.Integer; 
 	return false;
 }
@@ -36,28 +44,28 @@ inline bool operator!=(const VNDSVariable& lhs, const VNDSVariable& rhs)
 inline bool operator< (const VNDSVariable& lhs, const VNDSVariable& rhs)
 { 
 
-	if((lhs.Type == VNDSVariableType::Integer) && (rhs.Type == VNDSVariableType::Integer))
+	if((lhs.IsInteger()) && (rhs.IsInteger()))
 		return lhs.Data.Integer < rhs.Data.Integer;
 	return false;
 }
 inline bool operator> (const VNDSVariable& lhs, const VNDSVariable& rhs)
 { 
 
-	if((lhs.Type == VNDSVariableType::Integer) && (rhs.Type == VNDSVariableType::Integer))
+	if((lhs.IsInteger()) && (rhs.IsInteger()))
 		return lhs.Data.Integer > rhs.Data.Integer;
 	return false;
 }
 inline bool operator<= (const VNDSVariable& lhs, const VNDSVariable& rhs)
 { 
 
-	if((lhs.Type == VNDSVariableType::Integer) && (rhs.Type == VNDSVariableType::Integer))
+	if((lhs.IsInteger()) && (rhs.IsInteger()))
 		return lhs.Data.Integer <= rhs.Data.Integer;
 	return false;
 }
 inline bool operator>= (const VNDSVariable& lhs, const VNDSVariable& rhs)
 { 
 
-	if((lhs.Type == VNDSVariableType::Integer) && (rhs.Type == VNDSVariableType::Integer))
+	if((lhs.IsInteger()) && (rhs.IsInteger()))
 		return lhs.Data.Integer >= rhs.Data.Integer;
 	return false;
 }
