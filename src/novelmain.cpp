d8423181605dac67e92e2e46be7655a623d8a225
bakagamedev <noel.abercrombie@hotmail.com>
2017-11-20 19:29:09 +0000

    Return to menu

--- src/novelmain.cpp
+++ src/novelmain.cpp
@@ -7,11 +7,18 @@ NovelMain::NovelMain(NovelHeader Header)
 
 void NovelMain::Run()
 {
-	bool Ready = false;
 	vita2d_pgf * pgf = vita2d_load_default_pgf();	//Font!
-	
+
+	SceCtrlData GamePad, GamePadLast;
+	bool Ready = false;
 	while(!Ready)
 	{
+		sceCtrlPeekBufferPositive(0, &GamePad, 1);
+		if(GamePad.buttons != 0)
+		{
+			Ready = true;
+		}
+
 		vita2d_start_drawing();
 		vita2d_clear_screen();
 		vita2d_pgf_draw_text(pgf, 0,25,RGBA8(255,255,0,255), 1.0f, Header.Name.c_str());
