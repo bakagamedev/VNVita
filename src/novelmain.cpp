03586965b81d649369d109b7c993ec65d5ce76fc
bakagamedev <noel.abercrombie@hotmail.com>
2017-11-21 14:06:00 +0000

    Move main novel loop

--- src/novelmain.cpp
+++ src/novelmain.cpp
@@ -3,29 +3,26 @@
 NovelMain::NovelMain(NovelHeader Header)
 {
 	this->Header = Header;
+	StatusCode = StatusType::OK;
+}
+
+NovelMain::~NovelMain()
+{
+	vita2d_wait_rendering_done();
+	vita2d_free_pgf(pgf);
 }
 
 void NovelMain::Run()
 {
-	vita2d_pgf * pgf = vita2d_load_default_pgf();	//Font!
-
-	SceCtrlData GamePad, GamePadLast;
-	bool Ready = false;
-	while(!Ready)
+	sceCtrlPeekBufferPositive(0, &GamePad, 1);
+	if(GamePad.buttons != 0)
 	{
-		sceCtrlPeekBufferPositive(0, &GamePad, 1);
-		if(GamePad.buttons != 0)
-		{
-			Ready = true;
-		}
-
-		vita2d_start_drawing();
-		vita2d_clear_screen();
-		vita2d_pgf_draw_text(pgf, 0,25,RGBA8(255,255,0,255), 1.0f, Header.Name.c_str());
-		vita2d_end_drawing();
-		vita2d_swap_buffers();
+		StatusType::Finished;
 	}
 
-	vita2d_wait_rendering_done();
-	vita2d_free_pgf(pgf);
+	vita2d_start_drawing();
+	vita2d_clear_screen();
+	vita2d_pgf_draw_text(pgf, 0,25,RGBA8(255,255,0,255), 1.0f, Header.Name.c_str());
+	vita2d_end_drawing();
+	vita2d_swap_buffers();
 }
--- src/novelmain.h
+++ src/novelmain.h
@@ -1,13 +1,18 @@
 #pragma once
+#include "defines.h"
 #include "novelheader.h"
 
 class NovelMain
 {
 private:
 	NovelHeader Header; 
+
+	vita2d_pgf * pgf = vita2d_load_default_pgf();	//Font!
+	SceCtrlData GamePad, GamePadLast;
 public:
 	StatusType StatusCode;
 
 	NovelMain(NovelHeader Header);
+	~NovelMain();
 	void Run();
 };
