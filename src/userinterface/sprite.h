61f958a28bd3b9eb39822a3c5250505da8166756
bakagamedev <noel.abercrombie@hotmail.com>
2018-01-29 16:14:30 +0000

    Added empty ctor to sprite

--- src/userinterface/sprite.h
+++ src/userinterface/sprite.h
@@ -8,6 +8,7 @@ class UISprite
 private:
 	std::shared_ptr<vita2d_texture> texture;
 public:
+	UISprite(void) = default;
 	UISprite(std::shared_ptr<vita2d_texture> Texture);	
 	UISprite(const std::string& Path);
 	UISprite(const std::string& Path, uint32_t Flags);
